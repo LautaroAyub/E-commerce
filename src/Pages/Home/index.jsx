090bcf123cb615177744c5f514ed6e6c93a021f0
Lautaro Emanuel Ayub <lautiayub@hotmail.com>
2024-04-20 18:42:01 -0300

    Added API query and 'cards' creation in store

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,15 +1,32 @@
+import { useState, useEffect } from "react"
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card/indes"
 function Home() {
+    const urlAPI = 'https://fakestoreapi.com';
+    const [items, setItems] = useState(null)
+
+    useEffect(() => {
+        fetch(`${urlAPI}/products`)
+            .then(response => response.json())
+            .then(data => setItems(data))
+            .catch(err=>console.log(err))
+
+    }, [])
 
     return (
         <Layout>
-        Home
-        <Card/>
+            Home
+            <div className="grid gap-10 grid-cols-4 w-full max-w-screen-lg">
+                {
+                    items?.map((item) => (
+                        <Card key={item.id} data={item} />
+                    ))
+                }
+            </div>
+
         </Layout>
 
     )
-  }
-  
-  export default Home
-  
+}
+
+export default Home
